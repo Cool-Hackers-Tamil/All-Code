6f3023757e273330b0c2278db412843df2e7f6ca
Harshad <67547964+Cool-Hackers-Tamil@users.noreply.github.com>
2021-09-12 23:05:13 +0530

    Add files via upload

--- update.py
+++ update.py
@@ -1,13 +1,31 @@
 import os, time
 def sys(txt):
   os.system(txt)
-print('[*] Updating')
-sys('pkg update -y &>> Update')
-print('[*] All Most Done')
-sys('apt-get update -y &>> Update')
-print('[*] Upgrading')
-sys('pkg upgrade -y >> Update')
-print('[*] Upgrading Some Packages')
-sys('apt-get upgrade -y >> Update')
-print('[*] Your Termux UP To Date')
-os.system('rm Update')
+
+update="""
+echo '[*] Updating...'
+apt update -y &>> Update.log
+echo '[*] All Most Done...'
+pkg update -y &>> Update.log
+rm Update.log
+"""
+upgrade="""
+echo '[*] Upgrading...'
+pkg upgrade -y &>> Upgrade.log 
+echo '[*] Upgrading Some Packages...'
+apt-get upgrade -y &>> Upgrade.log
+echo '[*] Your Termux UP To Date'
+rm Upgrade.log
+"""
+os.system(update+upgrade)
+# Old Code
+#print('[*] Updating')
+#sys('pkg update -y &>> Update')
+#print('[*] All Most Done')
+#sys('apt-get update -y &>> Update')
+#print('[*] Upgrading')
+#sys('pkg upgrade -y >> Update')
+#print('[*] Upgrading Some Packages')
+#sys('apt-get upgrade -y >> Update')
+#print('[*] Your Termux UP To Date')
+#os.system('rm Update')
